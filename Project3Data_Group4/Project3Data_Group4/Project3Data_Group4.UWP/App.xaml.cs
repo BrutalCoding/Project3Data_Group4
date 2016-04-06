4276935639793d6468025339f83789a102480bab
BrutalCoding <dhbreedeveld@gmail.com>
2016-04-07 01:35:39 +0200

    Added 91 changes, might wanna revert back..

--- Project3Data_Group4/Project3Data_Group4/Project3Data_Group4.UWP/App.xaml.cs
+++ Project3Data_Group4/Project3Data_Group4/Project3Data_Group4.UWP/App.xaml.cs
@@ -43,7 +43,7 @@ namespace Project3Data_Group4.UWP
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
